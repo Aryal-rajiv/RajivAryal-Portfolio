b6b2674acbbf62f8a39a1db4915ff31cf74762f3
Aryal-rajiv <rajivary1@gmail.com>
2025-01-14 11:55:02 +0545

    pushing the commit

--- server.js
+++ server.js
@@ -13,37 +13,49 @@ app.use(express.static('public'));
 
 //POSt route to handle submission
 app.post('/submit-form', (req, res) =>{
-    const {name, email, subject, message} = req.body;
-})
+    const { name, email, subject, message } = req.body;
 
-//Create a Nodemailer transporter using Gmail
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth:{
-        user: process.env.GMAIL_USER, //Gmail user
-        pass: process.env.GMAIL_PASS  // Gmail API pass
-    }
+
+    //Create a Nodemailer transporter using Gmail
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: false,
+        auth:{
+            user: process.env.EMAIL_USER, //Gmail user
+            pass: process.env.EMAIL_PASS  // Gmail API pass
+        }
+    });
+
+
+    transporter.verify(function(error, success){
+        if (error){
+        } else{
+            console.log("Server is ready to take our messages");
+        }
+    })
+
+    //Email options
+    const mailoptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.YOUR_EMAIL,
+        subject: 'New message from${name}',
+        text: `The uername is ${name} email is ${email} & subject is ${subject} and the message is ${message}`
+    };
+
+    //Send email
+    transporter.sendMail(mailoptions, (error, info) => {
+        if (error){
+            console.error('Error sending email:', error);
+            res.status(500).send('Error sending email');
+        } else{
+            console.log('Email sent:', info.response);
+            res.status(200).send('Email sent successfully');
+        }
+
+    });
 });
-
-//Email options
-const mailoptions = {
-    from: process.env.GMAIL_USER,
-    to: process.env.YOUR_EMAIL,
-    subject: 'New message from${name}',
-    text: message
-};
-
-//Send email
-transporter.sendMail(mailoptions, (error, info)) => {
-    if (error){
-        console.error('Error sending email:', error);
-        res.status(500).send('Error sending email');
-    } else{
-        console.log('Email sent:', info.response);
-        res.status(200).send('Email sent successfully');
-    }
-
-}
 
 //start the server
 app.listen(port, () => {
